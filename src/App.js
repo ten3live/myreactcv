4b7aa60c2ec5fd5a0775bdaf4437863798c9fe3f
Sajjad Ali <ten3live@gmail.com>
2024-12-05 10:50:10 +0500

    All Changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
-import React from "react";
+import React from 'react'
 
-export default function Form() {
+export default function App() {
   return (
-    <form name="contact" method="post">
-      <input type="hidden" name="form-name" value="contact" />
-      <p>
-        <label htmlFor="name">Name</label> <br />
-        <input type="text" id="name" name="name" required />
-      </p>
-      <p>
-        <label htmlFor="email">Email</label> <br />
-        <input type="email" id="email" name="email" required />
-      </p>
-      <p>
-        <label htmlFor="message">Message</label> <br />
-        <textarea id="message" name="message" required></textarea>
-      </p>
-      <p>
-        <input type="submit" value="Submit message" />
-      </p>
-    </form>
-  );
+    <div>App</div>
+  )
 }
