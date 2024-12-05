eea09c21ee9348f1d1a621746dca92304131ef68
Sajjad Ali <ten3live@gmail.com>
2024-12-05 11:48:42 +0500

    did

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.Fragment>
     <App />
-  </React.StrictMode>,
+  </React.Fragment>,
   document.getElementById('root')
 );
 
-reportWebVitals(sendToVercelAnalytics);
+
