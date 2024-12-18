89a62d3bba5221010fdf21c7f928efb8db0f23ec
Sajjad Ali <ten3live@gmail.com>
2024-12-18 09:30:24 +0500

    First

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,23 +1,6 @@
 /* HTML document is loaded. DOM is ready.
 -------------------------------------------*/
 $(function () {
-  /* start typed element */
-  //http://stackoverflow.com/questions/24874797/select-div-title-text-and-make-array-with-jquery
-  var subElementArray = $.map($(".sub-element"), function (el) {
-    return $(el).text();
-  });
-  $(".element").typed({
-    strings: subElementArray,
-    typeSpeed: 30,
-    contentType: "html",
-    showCursor: false,
-    loop: true,
-    loopCount: true,
-  });
-  /* end typed element */
-
-  /* Smooth scroll and Scroll spy (https://github.com/ChrisWojcik/single-page-nav)    
-    ---------------------------------------------------------------------------------*/
   $(".templatemo-nav").singlePageNav({
     offset: $(".templatemo-nav").height(),
     filter: ":not(.external)",
